e987ca52613243940b5b6ef4c2da818796fc4164
artginzburg <art.ginzburg@ya.ru>
2021-07-30 18:17:01 +0300

    more user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,13 @@
 const User = require("../models/user");
 
-module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((users) =>
+      res.send(
+        "Данные не прошли валидацию. Либо произошло что-то совсем немыслимое"
+      )
+    );
 };
 
 module.exports.findUser = (req, res) => {
@@ -18,12 +20,20 @@ module.exports.findUser = (req, res) => {
     );
 };
 
-module.exports.getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((users) =>
-      res.send(
-        "Данные не прошли валидацию. Либо произошло что-то совсем немыслимое"
-      )
-    );
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: "Произошла ошибка" }));
+};
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user.id, { name, about });
+};
+
+module.exports.updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user.id, { avatar });
 };
