17a31b23936e82e913b0197f95f2ecc408216ade
artginzburg <art.ginzburg@ya.ru>
2021-08-01 19:04:29 +0300

    fix url validator, add .prettierrc

--- models/helpers/validateUrl.js
+++ models/helpers/validateUrl.js
@@ -1,7 +1,9 @@
 const regUrl = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
 
+const urlValidator = (val) => regUrl.test(val);
+
 const validateUrl = {
-  validator: regUrl.test,
+  validator: urlValidator,
   message: 'Не похоже на ссылку',
 };
 
